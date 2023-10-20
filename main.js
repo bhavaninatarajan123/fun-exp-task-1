e27d479e09053786f9df0b9ac18316b3b21ed65f
priyankacute <bhavanimurugesh2000@gmail.com>
2023-10-20 19:04:43 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,10 @@
+let a=prompt("enter the height");
+let b=prompt("enter the width");
+
+let rect=function(a,b){
+    
+    let c=a*b;
+    document.write(`the area of rectangle is ${c}`)
+
+}
+rect(a,b);
